fdac188492bfd40946ebb769c111392e827ce1e3
Gabriel Couto <gabriel.gregorio@devmagic.com.br>
2023-02-07 12:05:57 -0300

    Continuação na revisão de Angular

---
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PhotoService } from '../photo/photo.service';
+
+@Component({
+  selector: 'app-photo-list',
+  templateUrl: './photo-list.component.html',
+  styleUrls: ['./photo-list.component.css']
+})
+export class PhotoListComponent implements OnInit {
+
+  photos: Object[] = [];
+
+  constructor(private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const userName = this.activatedRoute.snapshot.params.userName;
+    console.log(userName);
+    
+    this.photoService.listFromUser(userName).subscribe(photos => {
+      console.log(photos[0].userId);
+      this.photos = photos
+    });
+
+  }
+}
