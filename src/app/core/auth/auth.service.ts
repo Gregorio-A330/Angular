cf12a4115db2577c53407914b8f0c97a6cebdff7
Gabriel Couto <gabriel.gregorio@devmagic.com.br>
2023-02-16 14:41:25 -0300

    Aula 02 - Autenticação e o papel do token

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators'
+import { TokenService } from '../token/token.service';
 
 
 const API_URL = 'http://localhost:3000'
@@ -9,10 +11,27 @@ const API_URL = 'http://localhost:3000'
 
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService) { }
 
 
-  authenticate(userName: string, password: string){
-    return this.http.post(API_URL + '/user/login', {userName: userName, password: password})
+  authenticate(userName: string, password: string) {
+    return this.http
+      .post(
+        API_URL + '/user/login',
+        { userName: userName, password: password },
+        { observe: 'response' }) //vou ter acesso ao cabeçalho a tudo que tem direito da resposta 
+      .pipe(tap(res => {
+        const authToken = res.headers.get('x-access-token');
+        this.tokenService.setToken(authToken)
+        console.log(`User ${userName} authenticated with token ${authToken}`)
+      }))
+
   }
 }
+
+//informação de um token de login no retorno da requisição
+// x-access-token: ??????????????
+
+// estrair esta informação do access token e validar o usuário
---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'ap-header',
+    templateUrl: './header.component.html'
+})
+export class HeaderComponent implements OnInit {
+    constructor() {
+        
+    }
+    
+    ngOnInit(): void {
+        throw new Error("Method not implemented.");
+    }
+}
